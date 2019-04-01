f6a7023eaebb04d62fb2d94b6fc8bea3f2fef61f
Tanajee <tanajee.yedage@techprimelab.com>
2019-04-01 09:42:57 +0530

    poocar seARCH

--- src/pages/search-pool/search-pool.ts
+++ src/pages/search-pool/search-pool.ts
@@ -5,6 +5,7 @@ import {PoolCar} from "../../models/poolcar.interface";
 import {Item} from "../../models/item";
 import {Profile} from "../../models/profile.interface";
 import {HttpClient} from "@angular/common/http";
+import {AddpoolcarInterface} from "../../models/addpoolcar.interface";
 /**
  * Generated class for the SearchPoolPage page.
  *
@@ -20,7 +21,7 @@ import {HttpClient} from "@angular/common/http";
 export class SearchPoolPage {
   pooCarDetails = { } as PoolCar;
   poolcarData: PoolCar[];
-  poolcars: PoolCar[];
+  poolcars:  Array<PoolCar> = [];
   cars:Observable<any>;
   constructor(public navCtrl: NavController, public navParams: NavParams,
               private http: HttpClient, private toastCtrl: ToastController) {
@@ -36,6 +37,17 @@ export class SearchPoolPage {
     this.navCtrl.push('ItemDetailPage', {
       item: item
     });
+  }
+  search(){
+    this.poolcars = [];
+    this.poolcarData.forEach((item1, index) => {
+      if(item1.from == this.pooCarDetails.from){
+        if(item1.to == this.pooCarDetails.to){
+          this.poolcars.push(item1);
+        }
+      }
+    })
+    console.log(this.poolcars);
   }
 
 }
